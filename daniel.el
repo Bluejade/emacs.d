d4f58dabd241fe9044044726ea77ceeea4ecfb1b
Daniel Nelson <dnelson@bluejade.com>
2009-08-31 16:00:39 -0500

    Removed commented out old includes, unnecessary because of the Emacs Starter Kit and Elpa

--- daniel.el
+++ daniel.el
@@ -1,22 +1,3 @@
-;; (require 'haml-mode)
-
-
-;; Ruby on Rails mode
-;;(let* ((this-file (or load-file-name buffer-file-name))
-;;       (this-dir (file-name-directory this-file))
-;;       (rails-path (concatenate 'string this-dir "daniel/rails-reloaded")))
-;;  (setq load-path (cons rails-path load-path)))
-
-;;(require 'rails-autoload)
-
-;;(require 'snippet)
-;;(require 'find-recursive)
-;;(require 'rails)
-;; end:Ruby on Rails mode
-
-;Allows syntax highlighting to work, among other things
-;(global-font-lock-mode 1)
-
 (require 'ecb)
 
 ;;Omit this if using Emacs >22 or if not using git
