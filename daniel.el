7cf2b20f38c1bee4bfde905d0f507925f3197fce
Daniel Nelson <dnelson@bluejade.com>
2009-08-31 16:38:11 -0500

    Added Cucumber minor mode

--- daniel.el
+++ daniel.el
@@ -7,3 +7,16 @@
 ;; (require 'git)
 ;; (autoload 'git-blame-mode "git-blame"
 ;;   "Minor mode for incremental blame for Git." t)
+
+;; (autoload 'test-case-mode "test-case-mode" nil t)
+;; (autoload 'enable-test-case-mode-if-test "test-case-mode")
+;; (autoload 'test-case-find-all-tests "test-case-mode" nil t)
+;; (autoload 'test-case-compilation-finish-run-all "test-case-mode")
+
+;; ;; Enable test-case-mode automatically when opening test files
+;; (add-hook 'find-file-hook 'enable-test-case-mode-if-test)
+
+;; Cucumber mode
+(add-to-list 'load-path "~/.emacs.d/daniel/cucumber-mode")
+(autoload 'cucumber-mode "cucumber-mode" "Mode for editing cucumber files" t)
+(add-to-list 'feature-mode '("\.feature$" . cucumber-mode))
