8cd19586ed0da77f2ba5ad409a8e40213303ddcb
Daniel Nelson <dnelson@bluejade.com>
2009-08-31 15:07:21 -0500

    Switched out emacs-rails for emacs-rails-reloaded because of a bug in emacs-rails

--- daniel.el
+++ daniel.el
@@ -1,54 +1,18 @@
-(setq keybindings-file "~/.emacs.d/keybindings.el")
-(load keybindings-file)
+;; (require 'haml-mode)
 
 
-;;use Twilight color scheme
-(require 'color-theme)
-(eval-after-load "color-theme"
-  '(progn
-     ;;if this ever fails, include the following line
-     ;;(color-theme-initialize)
-     (color-theme-twilight)))
+;; Ruby on Rails mode
+(let* ((this-file (or load-file-name buffer-file-name))
+       (this-dir (file-name-directory this-file))
+       (rails-path (concatenate 'string this-dir "daniel/rails-reloaded")))
+  (setq load-path (cons rails-path load-path)))
 
-
-;;maximize the window
-(defun toggle-fullscreen ()
-  (interactive)
-  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
-	    		 '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
-  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
-	    		 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
-)
-(toggle-fullscreen)
-
-;;disable system beep
-(setq visible-bell t)
-(global-hl-line-mode)
-;;highlight the marked region & un-mark upon buffer modification
-(transient-mark-mode t)
-
-;;enable Emacs to copy to and from clipboard using normal
-;;kill and yank commands
-(setq x-select-enable-clipboard t)
-
-;;for better buffer navigation
-(require 'ido)
-(ido-mode t)
-
-(require 'haml-mode)
-
-;; ;;ruby and ruby on rails
-;;(require 'snippet)
-;;(require 'find-files-in-project.el)
-;;(require 'inf-ruby)
-;;(setq load-path (cons "~/.emacs.d/daniel/rails" load-path))
-;;(add-to-list 'load-path "~/.emacs.d/daniel/rails")
-;;(require 'rails)
-;;end:ruby and ruby on rails
-
-
-(setq load-path (cons (expand-file-name "~/.emacs.d/daniel/rails-reloaded") load-path))
 (require 'rails-autoload)
+
+;;(require 'snippet)
+;;(require 'find-recursive)
+;;(require 'rails)
+;; end:Ruby on Rails mode
 
 ;Allows syntax highlighting to work, among other things
 ;(global-font-lock-mode 1)
@@ -56,9 +20,9 @@
 (require 'ecb)
 
 ;;Omit this if using Emacs >22 or if not using git
-(setq load-path (cons (expand-file-name "/usr/share/doc/git-core/contrib/emacs") load-path))
-(require 'vc-git)
-(when (featurep 'vc-git) (add-to-list 'vc-handled-backends 'git))
-(require 'git)
-(autoload 'git-blame-mode "git-blame"
-  "Minor mode for incremental blame for Git." t)
+;; (setq load-path (cons (expand-file-name "/usr/share/doc/git-core/contrib/emacs") load-path))
+;; (require 'vc-git)
+;; (when (featurep 'vc-git) (add-to-list 'vc-handled-backends 'git))
+;; (require 'git)
+;; (autoload 'git-blame-mode "git-blame"
+;;   "Minor mode for incremental blame for Git." t)
