b201add4cb74e4e95a47514cc484999a2d003164
Daniel Nelson <daniel@Optiplex760.(none)>
2009-09-09 15:47:36 -0500

    Default ECB to a standard layout (I didn't realize the compilation window could be disabled for the pre-builts) so that we can include custom.el, which means we don't have to customize ECB for other installations.

---
+++ custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(ecb-layout-name "left3")
+ '(ecb-options-version "2.32")
+ '(ecb-source-path (quote ("~/Work/MindLev/mindlev" "~/Work/ISB/isbtools")))
+ '(ecb-tip-of-the-day nil)
+ '(ecb-windows-width 0.25))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
