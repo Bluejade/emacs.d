eec2f793b45b6db88aedfc097450e9df1e85d72a
Daniel Nelson <daniel@Optiplex760.(none)>
2010-01-20 10:07:02 -0600

    replaced use of xsel with xclip for copying contents between emacs and other applications because xsel truncated long clipboard contents

--- daniel.el
+++ daniel.el
@@ -25,7 +25,8 @@
 (add-hook 'feature-mode-hook (lambda () (setq modelinepos-column-limit 300)))
 (add-hook 'haml-mode-hook (lambda () (setq modelinepos-column-limit 300)))
 
-
+(require 'xclip)
+(turn-on-xclip)
 ;; (require 'ecb)
 
 ;; Ruby electric mode interfered with auto indentation, so that
@@ -49,54 +50,3 @@
 ;; and load it
 (require 'feature-mode)
 (add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
-
-
-;; The following is from http://hugoheden.wordpress.com/2009/03/08/copypaste-with-emacs-in-terminal/
-;; If emacs is run in a terminal, the clipboard- functions have no
-;; effect. Instead, we use of xsel, see
-;; http://www.vergenet.net/~conrad/software/xsel/ -- "a command-line
-;; program for getting and setting the contents of the X selection"
-(unless window-system
-  ;; Callback for when user cuts
-  (defun xsel-cut-function (text &optional push)
-
-    ;; Insert text to temp-buffer, and "send" content to xsel
-    ;; stdin
-    (with-temp-buffer
-      (insert text)
-
-      ;; I prefer using the "clipboard" selection (the
-      ;; one the
-      ;; typically is used by c-c/c-v) before the
-      ;; primary selection
-      ;; (that uses mouse-select/middle-button-click)
-      (call-process-region (point-min)
-                           (point-max)
-                           "xsel" nil 0 nil "--clipboard" "--input")))
-
-  ;; Call back for when user pastes
-  (defun xsel-paste-function ()
-
-    ;; Find out what is current selection by xsel. If it is
-    ;; different
-    ;; from the top of the kill-ring (car kill-ring), then
-    ;; return
-    ;; it. Else, nil is returned, so whatever is in the top of
-    ;; the
-    ;; kill-ring will be used.
-    (let ((xsel-output (shell-command-to-string "xsel --clipboard --output")))
-
-      (unless (string= (car kill-ring)
-                       xsel-output)
-
-        xsel-output)))
-
-  ;; Attach callbacks to hooks
-  (setq interprogram-cut-function 'xsel-cut-function)
-
-  (setq interprogram-paste-function 'xsel-paste-function)
-
-  ;; Idea from
-  ;; http://shreevatsa.wordpress.com/2006/10/22/emacs-copypaste-and-x/
-  ;; http://www.mail-archive.com/help-gnu-emacs@gnu.org/msg03577.html
-  )
